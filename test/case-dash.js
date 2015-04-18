5b015fe9837870fe236d3397a361b5d12137aea6
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-18 19:01:10 +0000

    Use ES6 in tests

--- test/case-dash.js
+++ test/case-dash.js
@@ -15,33 +15,25 @@ describe('case-dash', function () {
     ]
 
   describe('#parse()', function () {
-    tests.forEach(function (pair) {
-      var name = pair[0],
-        parsed = pair[1]
-
+    for (let [name, parsed] of tests) {
       it(JSON.stringify(name) + ' -> ' + JSON.stringify(parsed), function () {
         assert.deepEqual(caseDash.parse(name), parsed)
       })
-    })
+    }
   })
 
   describe('#stringify()', function () {
-    tests.forEach(function (pair) {
-      var name = pair[0],
-        parsed = pair[1]
-
+    for (let [name, parsed] of tests) {
       it(JSON.stringify(parsed) + ' -> ' + JSON.stringify(name), function () {
         assert.equal(caseDash.stringify(parsed), name)
       })
-    })
+    }
   })
 
   describe('#is()', function () {
-    tests.forEach(function (pair) {
-      var name1 = pair[0],
-        parsed = pair[1],
-        name2 = pair[1].join('#'),
-        val1 = parsed.length > 1
+    for (let [name1, parsed] of tests) {
+      const name2 = parsed.join('#')
+      const val1 = parsed.length > 1
 
       it(JSON.stringify(name1) + ' (' + val1 + ')', function () {
         assert.equal(caseDash.is(name1), val1)
@@ -49,6 +41,6 @@ describe('case-dash', function () {
       it(JSON.stringify(name2) + ' (false)', function () {
         assert.equal(caseDash.is(name2), false)
       })
-    })
+    }
   })
 })
