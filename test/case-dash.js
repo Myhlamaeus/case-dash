136b51a03d7510ddf4193b64708c0a4ada2adfe2
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-04-18 18:59:05 +0000

    Fix standard errors in tests

--- test/case-dash.js
+++ test/case-dash.js
@@ -1,53 +1,54 @@
-import caseDash from "../case-dash";
-import assert from "assert";
+/* global it, describe */
 
+import caseDash from '../case-dash';
+import assert from 'assert';
 
-describe("case-dash", function() {
-    var tests = [
-            ["T", ["T"]],
-            ["t", ["t"]],
-            ["Test", ["Test"]],
-            ["test", ["test"]],
-            ["this-Is-A-Test", ["this", "Is", "A", "Test"]],
-            ["This-Is-A-Test", ["This", "Is", "A", "Test"]],
-            ["THIS-Is-A-Test", ["THIS", "Is", "A", "Test"]]
-        ];
+describe('case-dash', function () {
+  const tests = [
+      ['T', ['T']],
+      ['t', ['t']],
+      ['Test', ['Test']],
+      ['test', ['test']],
+      ['this-Is-A-Test', ['this', 'Is', 'A', 'Test']],
+      ['This-Is-A-Test', ['This', 'Is', 'A', 'Test']],
+      ['THIS-Is-A-Test', ['THIS', 'Is', 'A', 'Test']]
+    ]
 
-    describe("#parse()", function() {
-        tests.forEach(function(pair) {
-            var name = pair[0],
-                parsed = pair[1];
+  describe('#parse()', function () {
+    tests.forEach(function (pair) {
+      var name = pair[0],
+        parsed = pair[1]
 
-            it(JSON.stringify(name) + " -> " + JSON.stringify(parsed), function() {
-                assert.deepEqual(caseDash.parse(name), parsed);
-            });
-        });
-    });
+      it(JSON.stringify(name) + ' -> ' + JSON.stringify(parsed), function () {
+        assert.deepEqual(caseDash.parse(name), parsed)
+      })
+    })
+  })
 
-    describe("#stringify()", function() {
-        tests.forEach(function(pair) {
-            var name = pair[0],
-                parsed = pair[1];
+  describe('#stringify()', function () {
+    tests.forEach(function (pair) {
+      var name = pair[0],
+        parsed = pair[1]
 
-            it(JSON.stringify(parsed) + " -> " + JSON.stringify(name), function() {
-                assert.equal(caseDash.stringify(parsed), name);
-            });
-        });
-    });
+      it(JSON.stringify(parsed) + ' -> ' + JSON.stringify(name), function () {
+        assert.equal(caseDash.stringify(parsed), name)
+      })
+    })
+  })
 
-    describe("#is()", function() {
-        tests.forEach(function(pair) {
-            var name1 = pair[0],
-                parsed = pair[1],
-                name2 = pair[1].join("#"),
-                val1 = parsed.length > 1;
+  describe('#is()', function () {
+    tests.forEach(function (pair) {
+      var name1 = pair[0],
+        parsed = pair[1],
+        name2 = pair[1].join('#'),
+        val1 = parsed.length > 1
 
-            it(JSON.stringify(name1) + " (" + val1 + ")", function() {
-                assert.equal(caseDash.is(name1), val1);
-            });
-            it(JSON.stringify(name2) + " (false)", function() {
-                assert.equal(caseDash.is(name2), false);
-            });
-        });
-    });
-});
+      it(JSON.stringify(name1) + ' (' + val1 + ')', function () {
+        assert.equal(caseDash.is(name1), val1)
+      })
+      it(JSON.stringify(name2) + ' (false)', function () {
+        assert.equal(caseDash.is(name2), false)
+      })
+    })
+  })
+})
